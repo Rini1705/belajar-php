72dabb717ee011cb42f72c35df25fdc62e2c1303
Rini Maulida <rinimaulida0517@gmail.com>
2022-11-06 21:46:08 +0700

    php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$nama = "Rini Maulida";
+$usia = "20";
+
+echo "Nama saya <strong>$nama</strong>,Usia saya saat ini $usia tahun";
+
+$namaadik = "rizal";
+$umuradik = "17";
+
+echo "Nama adik saya <strong>$namaadik</strong>,Usia adik saya saat ini $umuradik tahun";
+
+
+
+?>
